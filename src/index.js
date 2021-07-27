817c8fb90ae0219fa468b46eb0b3616cba9bdfde
taka5095 <87805831+taka5095@users.noreply.github.com>
2021-07-28 01:17:44 +0900

    戻すボタンの直前まで

--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+  //完了ボタンの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //未完了リストから削除
+    deleteFromIncompletelist(completeButton.parentNode);
+    //押された完了ボタンの親タグ(div)を完了リストへ移動＆未完了リストから削除
+    //完了リストに追加
+    const addTarget = completeButton.parentNode;
+    //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    //div以下を初期化
+    addTarget.textContent = null;
+    //liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+    //戻すボタンの生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  //削除ボタンの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompletelist(deleteButton.parentNode);
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompletelist = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//完了リストから指定の要素を削除
+const deleteFromCompletelist = (target) => {
+  document.getElementById("complete-list").removeChild(target);
 };
 
 document
